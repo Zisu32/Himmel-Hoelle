1a8951f4883b8982fbf9bc74782842fab755b4b1
nhuhuent <nhuhuent84@gmx.de>
2024-03-29 21:04:25 +0100

    Life time

---
+++ Himmel-Hoelle/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100f; 
+    public float healthDecayRate = 0.5f; 
+    private float currentHealth; 
+    public TextMeshProUGUI healthText; 
+    public Image healthBar; 
+
+    void Start()
+    {
+        currentHealth = maxHealth; 
+    }
+
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+
+        currentHealth -= healthDecayRate * deltaTime;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+
+        UpdateUI();
+
+        if (currentHealth <= 0f)
+        {
+            Die(); // Xử lý sự kiện khi mạng sống hết
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player died!");
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+    }
+
+    void UpdateUI()
+    {
+        if (healthText != null)
+        {
+            healthText.text = "Health: " + Mathf.RoundToInt(currentHealth); 
+        }
+
+        if (healthBar != null)
+        {
+            healthBar.fillAmount = currentHealth / maxHealth; 
+        }
+    }
+}

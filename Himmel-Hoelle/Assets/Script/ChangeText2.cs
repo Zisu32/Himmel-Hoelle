4a8e5669414f52ed9bea129d13025bf526895b99
Zisu32 <pas_loves_nikes@hotmail.de>
2024-05-24 12:57:18 +0200

    Change UI added Char to Level Completed

--- Himmel-Hoelle/Assets/Script/ChangeText2.cs
+++ Himmel-Hoelle/Assets/Script/ChangeText2.cs
@@ -20,34 +20,34 @@ public class ChangeText2 : MonoBehaviour
                 switch (moveManager.currentIndex)
                 {
                     case 0:
-                        podestText.text = "Ich war tief im Wald und habe die Vögel singen gehört, als plötzlich der Rauch aufstieg. Es war furchtbar! Die Luft war erstickend, und ich konnte das Knistern der Flammen hören. Mein Herz brach, als ich sah, wie der geliebte Wald in Flammen stand. Es war, als ob ein Teil meiner Seele mit jedem Baum verbrannte.";
+                        podestText.text = "<b>YRA:</b>\n \"Ich war tief im Wald und habe die Vögel singen gehört, als plötzlich der Rauch aufstieg. Es war furchtbar! Die Luft war erstickend, und ich konnte das Knistern der Flammen hören. Mein Herz brach, als ich sah, wie der geliebte Wald in Flammen stand. Es war, als ob ein Teil meiner Seele mit jedem Baum verbrannte.\"";
                         break;
                     case 1:
-                        podestText.text = "Ich habe den ganzen Tag schon das Gefühl gehabt, dass etwas schlimmes passieren wird, aber ich konnte nichts tun, um es zu stoppen. Meine Knie zitterten vor Angst, als ich das Knistern der Flammen näher zu meinem Bau kommen hörte. Die Natur selbst schrie vor Schmerz, als der Wald in einem infernalischen Feuersturm versank. Es war ein Alptraum...";
+                        podestText.text = "<b>THALON:</b>\n \"Ich habe den ganzen Tag schon das Gefühl gehabt, dass etwas schlimmes passieren wird, aber ich konnte nichts tun, um es zu stoppen. Meine Knie zitterten vor Angst, als ich das Knistern der Flammen näher zu meinem Bau kommen hörte. Die Natur selbst schrie vor Schmerz, als der Wald in einem infernalischen Feuersturm versank. Es war ein Alptraum...\"";
                         break;
                     case 2:
-                        podestText.text = "Ich war mit Übungen meiner Lichtmagie beschäftigt, um endlich die Anerkennung zu bekommen, die mir niemand gewährt. Jemand ist ganz plötzlich an mir vorbei gerauscht und hat mich so aus der Fassung gebracht, dass ich in einen großen Laubhaufen fiel. Da hätte ich beinahe die Kontrolle über meine Magie verloren. Bei dieser Trockenheit hätte ein Funken ausgereicht, um ein Inferno zu verursachen!";
+                        podestText.text = "<b>ELARA:</b>\n \"Ich war mit meiner Lichtmagie beschäftigt, um die Anerkennung zu bekommen, die mir niemand gewährt. Jemand ist plötzlich an mir vorbei gerauscht und hat mich so aus der Fassung gebracht, dass ich in einen Laubhaufen fiel. Da hätte ich fast die Kontrolle verloren. Bei der Trockenheit hätte ein Funken ausgereicht, um ein Inferno zu verursachen!\"";
                         break;
                     case 3:
-                        podestText.text = "Die Bäume brannten viel zu schnell. Es muss Brandbeschleuniger gegeben haben. Der Geruch von verbranntem Holz und die Hitze der Flammen trieben mich zur Verzweiflung. Als Waldwächter fühle ich mich verpflichtet, den Wald zu beschützen, aber dieses Feuer war eine Macht, gegen die ich machtlos war. Wenn ich den Verantwortlichen erwische, wird er meine Wut zu spüren bekommen.";
+                        podestText.text = "<b>BRAN:</b>\n \"Die Bäume brannten zu schnell. Es muss Brandbeschleuniger gegeben haben. Als Waldwächter fühle ich mich verpflichtet, den Wald zu beschützen, aber dieses Feuer war eine Macht, gegen die ich machtlos war. Wenn ich den Verantwortlichen erwische, wird er meine Wut zu spüren bekommen.\"";
                         break;
                     case 4:
-                        podestText.text = "Ich versuchte, das Feuer mit meinem Wasser zu löschen, aber es war zu spät. Meine Tränen vermischten sich mit dem Wasser meiner Quellen, als ich versuchte, die Flammen zu ersticken. Aber das Feuer war stärker, als ich es je für möglich gehalten hätte. Ich fühle mich hilflos und traurig angesichts der Zerstörung, die über unseren geliebten Wald gekommen ist.";
+                        podestText.text = "<b>FINN:</b>\n \"Ich versuchte, das Feuer mit meinem Wasser zu löschen, aber es war zu spät. Aber das Feuer war stärker, als ich es je für möglich gehalten hätte. Ich fühle mich hilflos und traurig angesichts der Zerstörung, die über unseren geliebten Wald gekommen ist.\"";
                         break;
                     case 5:
-                        podestText.text = "Die Luft war voller Rauch und ich konnte kaum atmen. Es war wie ein Albtraum. Mein Herz weinte, als ich sah, wie die ersten Strahlen der Sonne durch den Rauch brachen und das Ausmaß der Zerstörung offenbarten. Meine Freundin Seraphina wäre fast umgekommen, so nah war sie dem Brandherd gekommen... Ein Glück, dass Elara bei ihr war! Die beiden verstehen sich in letzter Zeit ziemlich gut...";
+                        podestText.text = "<b>AURORA:</b>\n \"Die Luft war voller Rauch und ich konnte kaum atmen. Meine Freundin Seraphina wäre fast umgekommen, so nah war sie dem Brandherd gekommen... Ein Glück, dass Elara bei ihr war! Die beiden verstehen sich in letzter Zeit ziemlich gut...\"";
                         break;
                     case 6:
-                        podestText.text = "Ja, ich war in der Nähe des Brandherdes, aber ich habe versucht, das Feuer zu löschen! Ich habe mein Bestes getan, um den Wald zu retten, glaubt mir! Die Rußspuren auf meiner Kleidung sind ein Beweis dafür, wie hart ich gekämpft habe, um die Flammen zu ersticken. Mein Herz blutet vor Trauer über das, was geschehen ist, und ich werde alles in meiner Macht Stehende tun, um Gerechtigkeit für unseren geliebten Wald zu erreichen.";
+                        podestText.text = "<b>RAELLE:</b>\n \"Ja, ich war in der Nähe des Brandherdes, aber ich habe versucht, das Feuer zu löschen! Die Rußspuren auf meiner Kleidung sind ein Beweis dafür, wie hart ich gekämpft habe, um die Flammen zu ersticken. Ich werde alles in meiner Macht Stehende tun, um Gerechtigkeit für unseren geliebten Wald zu erreichen.\"";
                         break;
                     case 7:
-                        podestText.text = "Ich sah verdächtige Gestalten in der Nähe des Waldes, als ich die Sterne beobachtete. Die Schatten der Nacht verbargen ihre Identität, aber ihre bösen Absichten waren deutlich spürbar. Ich habe den Sternen geschworen, den Wald zu beschützen, und ich werde nicht zulassen, dass diejenigen, die das Feuer gelegt haben, ungestraft davonkommen.";
+                        podestText.text = "<b>ORION:</b>\n \"Ich sah verdächtige Gestalten in der Nähe des Waldes, als ich die Sterne beobachtete. Die Schatten der Nacht verbargen ihre Identität, aber ihre bösen Absichten waren deutlich spürbar. Ich habe den Sternen geschworen, den Wald zu beschützen, und ich werde nicht zulassen, dass diejenigen, die das Feuer gelegt haben, ungestraft davonkommen.\"";
                         break;
                     case 8:
-                        podestText.text = "Ich hatte unheimliches Glück, denn zum Zeitpunkt des Feuers war ich weit entfernt, ich habe kaum etwas mitbekommen – erst, als das Feuer bereits gelöscht war, kam ich her. Ich bin bloß froh, dass den Eichhörnchen dort nichts passiert ist – egal, wie viele Nüsse sie mir auch klauen, es wäre eine Schande, auf sie im Wald zu verzichten!";
+                        podestText.text = "<b>SERAPHINA:</b>\n \"Ich hatte unheimliches Glück, denn zum Zeitpunkt des Feuers war ich weit entfernt, ich habe kaum etwas mitbekommen – erst, als das Feuer bereits gelöscht war, kam ich her. Ich bin bloß froh, dass den Eichhörnchen dort nichts passiert ist – egal, wie viele Nüsse sie mir auch klauen, es wäre eine Schande, auf sie im Wald zu verzichten!\"";
                         break;
                     case 9:
-                        podestText.text = "Ich war zu beschäftigt damit, die Ruhe im Wald zu bewahren, um den Brand rechtzeitig zu bemerken. Die Verantwortung als Beschützer des Fey wiegt schwer auf meinen Schultern, und ich habe den Wald zu allen Zeiten im Blick. Doch selbst meine Wachsamkeit konnte nicht verhindern, dass das Feuer ausbrach und unsere geliebten Wälder verwüstete. Ich fühle mich zutiefst betrübt und machtlos angesichts dieser Tragödie.";
+                        podestText.text = "<b>AVALON:</b>\n \"Die Verantwortung als Beschützer des Fey wiegt schwer auf meinen Schultern, und ich habe den Wald zu allen Zeiten im Blick. Doch selbst meine Wachsamkeit konnte nicht verhindern, dass das Feuer ausbrach und unsere geliebten Wälder verwüstete. Ich fühle mich zutiefst betrübt und machtlos angesichts dieser Tragödie.\"";
                         break; 
                     default:
                         podestText.text = "Default Text";
--- Himmel-Hoelle/Assets/Script/ChangeText3.cs
+++ Himmel-Hoelle/Assets/Script/ChangeText3.cs
@@ -20,34 +20,34 @@ public class ChangeText3 : MonoBehaviour
                 switch (moveManager.currentIndex)
                 {
                     case 0:
-                        podestText.text = "Ich war die erste Person am Tatort... Ich war zur falschen Zeit am falschen Ort. Bitte glaubt mir, ich würde niemals jemandem absichtlich Schaden zufügen! Ich kann nicht einmal eine Mücke zerquetschen... Meine Brille beschlug vor Tränen, als ich die leblose Fey-Leiche sah. Es tut mir so leid für das Opfer und seine Familie. Ich hoffe, der wahre Täter wird bald gefasst.";
+                        podestText.text = "<b>ELIA:</b>\n \"Ich war die erste Person am Tatort... Ich war zur falschen Zeit am falschen Ort. Bitte glaubt mir, ich würde niemals jemandem absichtlich Schaden zufügen! Ich kann nicht einmal eine Mücke zerquetschen... Meine Brille beschlug vor Tränen, als ich die leblose Fey-Leiche sah. Es tut mir so leid für das Opfer und seine Familie. Ich hoffe, der wahre Täter wird bald gefasst.\"";
                         break;
                     case 1:
-                        podestText.text = "Ich mag vielleicht mysteriös wirken, aber ich versichere euch, ich habe nichts mit dem Tod dieser armen Seele zu tun. Ich war viel zu beschäftigt damit, die Geheimnisse des Feywoods zu erforschen. Meine Schritte hallten in der Stille wider, als ich am Tatort ankam. Möge das Echo der Wahrheit die Dunkelheit vertreiben.";
+                        podestText.text = "<b>THROK:</b>\n \"Ich mag vielleicht mysteriös wirken, aber ich versichere euch, ich habe nichts mit dem Tod dieser armen Seele zu tun. Ich war viel zu beschäftigt damit, die Geheimnisse des Feywoods zu erforschen. Meine Schritte hallten in der Stille wider, als ich am Tatort ankam. Möge das Echo der Wahrheit die Dunkelheit vertreiben.\"";
                         break;
                     case 2:
-                        podestText.text = "Ich habe wirklich keine Ahnung, was passiert ist. Ich war viel zu beschäftigt damit, die Schönheit dieser magischen Wälder zu bewundern, um irgendjemandem etwas anzutun! Vor ein paar Stunden sind meine grade angezüchteten Glühwürmchenlarven geschlüpft, das sind viele hungrige Larvenmäuler, um die ich mich kümmern muss! Ich hoffe, der Täter wird schnell gefasst und zur Rechenschaft gezogen.";
+                        podestText.text = "<b>LUNA:</b>\n \"Ich habe wirklich keine Ahnung, was passiert ist. Ich war viel zu beschäftigt damit, die Schönheit dieser magischen Wälder zu bewundern, um irgendjemandem etwas anzutun! Vor ein paar Stunden sind meine grade angezüchteten Glühwürmchenlarven geschlüpft! Ich hoffe, der Täter wird schnell gefasst und zur Rechenschaft gezogen.\"";
                         break;
                     case 3:
-                        podestText.text = "Ich bin vielleicht ein dunkler Fey, aber ich bin kein Mörder! Ihr könnt mir nicht einfach die Schuld zuschieben, nur weil ich anders aussehe! Mein Knurren erfüllte die Luft, ein anderes Raubtier so machtlos am Boden zu sehen. Sie war eine gute Werwölfin mit einer wunderschönen Singstimme. Dieser Vogel, der hatte ständig Streit mit ihr, ich bin sicher, dass er etwas damit zu tun hat.";
+                        podestText.text = "<b>GRIMMBO:</b>\n \"Ich bin vielleicht ein dunkler Fey, aber ich bin kein Mörder! Ihr könnt mir nicht einfach die Schuld zuschieben, nur weil ich anders aussehe! Sie hatte eine wunderschöne Singstimme. Dieser Vogeltyp, der hatte ständig Streit mit ihr, ich bin sicher, dass er etwas damit zu tun hat.\"";
                         break;
                     case 4:
-                        podestText.text = "Ich schwöre bei den Melodien der Wälder, ich habe nichts mit diesem schrecklichen Vorfall zu tun. Es brach mir das Herz, als ich die Leiche sah. Sie war die beste Sängerin des Waldes. wer soll den nun die musikalische Begleitung am Waldfest morgen übernehmen? Es ist eine Schande - Sie hätte sicher gewollt, dass es stattfindet. Ich schätze, dann muss ich mich wohl bereit erklären...";
+                        podestText.text = "<b>ARIA:</b>\n \"Ich schwöre bei den Melodien der Wälder, ich habe nichts mit diesem schrecklichen Vorfall zu tun. Sie war die beste Sängerin des Waldes. Wer soll den nun die musikalische Begleitung am Waldfest morgen übernehmen? Es ist eine Schande - Sie hätte sicher gewollt, dass es stattfindet. Ich schätze, dann muss ich mich wohl bereit erklären...\"";
                         break;
                     case 5:
-                        podestText.text = "Ich gebe zu, ich bin ein Schelm, aber ich bin kein Mörder! Ihr müsst mir glauben, ich hatte nichts mit dem Tod dieser armen Seele zu tun! Ich hörte allerdings Stimmen im Wald, eine männliche und drei weibliche. Ob es die Täter sind kann ich nicht genau sagen. Zudem hat sich Marcys Nachbar immer ziemlich über ihren Gesang aufgeregt...";
+                        podestText.text = "<b>ROGUE:</b>\n \"Ich gebe zu, ich bin ein Schelm, aber ich bin kein Mörder! Ihr müsst mir glauben, ich hatte nichts mit dem Tod dieser armen Seele zu tun! Ich hörte allerdings Stimmen im Wald, eine männliche und drei weibliche. Ob es die Täter sind kann ich nicht genau sagen. Zudem hat sich Marcys Nachbar immer ziemlich über ihren Gesang aufgeregt...\"";
                         break;
                     case 6:
-                        podestText.text = "Ich könnte nicht einmal einem Blatt Wind zufügen, geschweige denn einer anderen Kreatur! Bitte glaubt mir, ich bin unschuldig! Als es geschehen sein muss war ich gerade auf der Suche nach Ivy, um mir einen Rat für ein Elixier zu holen, doch sie war nirgends zu finden, auch in ihrer Hütte nicht... Ich mache mir solche Sorgen, dass die Täter noch in unserer Mitte weilen! Was, wenn Marcy nur eine von vielen war?";
+                        podestText.text = "<b>SYLPH:</b>\n \"Bitte glaubt mir, ich bin unschuldig! Als es geschehen sein muss war ich gerade auf der Suche nach Ivy, um mir einen Rat für ein Elixier zu holen, doch sie war nirgends zu finden, auch in ihrer Hütte nicht... Ich mache mir solche Sorgen, dass die Täter noch in unserer Mitte weilen! Was, wenn Marcy nur eine von vielen war?\"";
                         break;
                     case 7:
-                        podestText.text = "Meine arme, arme Marcy! Sie war mein Ein und Alles – Heiraten wollten wir bald! Jetzt habe ich niemanden mehr... Wer auch immer schuld an ihrem Tod ist, hat mir das letzte Glück genommen. Bitte sorgt dafür, dass demjenigen die rechte Strafe erteilt wird. Der Gedanke, dass ich dieser Person täglich begegnen könnte, widert mich an! Ich bin untröstlich!";
+                        podestText.text = "<b>VLADIMIR:</b>\n \"Meine arme, arme Marcy! Sie war mein Ein und Alles – Heiraten wollten wir bald! Jetzt habe ich niemanden mehr... Wer auch immer schuld an ihrem Tod ist, hat mir das letzte Glück genommen. Bitte sorgt dafür, dass demjenigen die rechte Strafe erteilt wird. Der Gedanke, dass ich dieser Person täglich begegnen könnte, widert mich an! Ich bin untröstlich!\"";
                         break;
                     case 8:
-                        podestText.text = "Ja, ich bin giftig, aber das bedeutet nicht, dass ich jemanden getötet habe! Ich war vollauf damit beschäftigt, meine Tränke zu brauen. Der arme Vladimir, ihm muss es schrecklich gehen! Ich als seine beste Freundin sollte mich direkt um ihn kümmern – er war doch so verliebt in Marcy, obwohl er so viel mehr verdient hätte... Er braucht sicher jemanden, der für ihn da ist.";
+                        podestText.text = "<b>IVY:</b>\n \"Ja, ich bin giftig, aber das bedeutet nicht, dass ich jemanden getötet habe! Ich war vollauf damit beschäftigt, meine Tränke zu brauen. Der arme Vladimir, ihm muss es schrecklich gehen! Ich als seine beste Freundin sollte mich direkt um ihn kümmern – er war doch so verliebt in Marcy, obwohl er so viel mehr verdient hätte... Er braucht sicher jemanden, der für ihn da ist.\"";
                         break;
                     case 9:
-                        podestText.text = "Ich mag vielleicht mysteriös wirken, aber ich versichere euch, ich habe nichts mit dem Tod dieser armen Seele zu tun. Außerdem war Marcy meine Nachbarin, die ich über alles respektierte.";
+                        podestText.text = "<b>ECHO:</b>\n \"Ich mag vielleicht mysteriös wirken, aber ich versichere euch, ich habe nichts mit dem Tod dieser armen Seele zu tun. Außerdem war Marcy meine Nachbarin, die ich über alles respektierte.";
                         break; 
                     default:
                         podestText.text = "Default Text";
